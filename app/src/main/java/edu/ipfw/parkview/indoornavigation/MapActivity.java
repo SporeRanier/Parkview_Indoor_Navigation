50d9de1caba9d31b8e3edfae8ca119b723ad712d
masonsnider30 <masonsnider30@gmail.com>
2017-12-09 17:36:54 -0500

    changed the theme colors to green, changed the app title, and started to add some test buttons and another placeholding activity for the map/wayfinding function

--- app/src/main/java/edu/ipfw/parkview/indoornavigation/MapActivity.java
+++ app/src/main/java/edu/ipfw/parkview/indoornavigation/MapActivity.java
@@ -3,15 +3,11 @@ package edu.ipfw.parkview.indoornavigation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class routeActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_route);
-    }
-
-    public void currentLocation(){
-
+        setContentView(R.layout.activity_map);
     }
 }
