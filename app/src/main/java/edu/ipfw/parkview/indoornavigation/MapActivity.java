42d763a4b381ee1a79d9cf8501c2fdc3eb7a3263
masonsnider30 <masonsnider30@gmail.com>
2017-12-09 21:28:00 -0500

    Changed the icon when it is installed and more testing stuff

--- app/src/main/java/edu/ipfw/parkview/indoornavigation/MapActivity.java
+++ app/src/main/java/edu/ipfw/parkview/indoornavigation/MapActivity.java
@@ -3,6 +3,8 @@ package edu.ipfw.parkview.indoornavigation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MapActivity extends AppCompatActivity {
 
