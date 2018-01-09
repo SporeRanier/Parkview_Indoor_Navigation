edb5cce8cde4618928cb63a44d8411e77df9e322
ddely <mcshdd01@students.ipfw.edu>
2018-01-09 16:48:11 -0500

    Prepared code to display map. Need to add keys

--- app/src/main/java/edu/ipfw/parkview/indoornavigation/MapActivity.java
+++ app/src/main/java/edu/ipfw/parkview/indoornavigation/MapActivity.java
@@ -6,11 +6,25 @@ import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.arubanetworks.meridian.Meridian;
+import com.arubanetworks.meridian.editor.EditorKey;
+import com.arubanetworks.meridian.maps.MapFragment;
+
 public class MapActivity extends AppCompatActivity {
 
+    //TODO: Put these strings in values/strings.xml
+    public static final EditorKey APP_KEY = EditorKey.forApp("need a number here");
+    public static final EditorKey MAP_KEY = EditorKey.forMap("need another number");
+    public static final String EDITOR_TOKEN = "Not a real token yet";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        Meridian.configure(this);
+        Meridian.getShared().initGoalsForLocation(APP_KEY.toString());
+        Meridian.getShared().setEditorToken(EDITOR_TOKEN);
+        EditorKey key = new EditorKey("needkey");
+        MapFragment mapFrag = MapFragment.newInstance(key, null);
     }
 }
