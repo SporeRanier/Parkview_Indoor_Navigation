1f99b33e9a46ec027e264916d30c3b27dc9d77a6
Kurtis Taylor <kjtaylor1995@gmail.com>
2018-01-11 17:51:05 -0500

    more work on base app, fiddling with keys but still not working

--- app/src/main/java/edu/ipfw/parkview/indoornavigation/MapActivity.java
+++ app/src/main/java/edu/ipfw/parkview/indoornavigation/MapActivity.java
@@ -6,25 +6,10 @@ import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import com.arubanetworks.meridian.Meridian;
-import com.arubanetworks.meridian.editor.EditorKey;
-import com.arubanetworks.meridian.maps.MapFragment;
-
 public class MapActivity extends AppCompatActivity {
-
-    //TODO: Put these strings in values/strings.xml
-    public static final EditorKey APP_KEY = EditorKey.forApp("f477d4c144c1541e6452b812bc3b194d4770b6c4");
-    public static final EditorKey MAP_KEY = EditorKey.forMap("6555052652625920", APP_KEY.getId());
-    //public static final String EDITOR_TOKEN = "Not a real token yet";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //EditorKey test = new EditorKey("ASD");
-        Meridian.configure(this);
-        Meridian.getShared().initGoalsForLocation(APP_KEY.toString());
-        //Meridian.getShared().setEditorToken(EDITOR_TOKEN);
-        //EditorKey key = new EditorKey("needkey");
-        MapFragment m = MapFragment.newInstance(APP_KEY, null);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
     }
