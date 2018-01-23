4dcf2abc0122bd975e3c6118d1ee7849e6e6e8fe
kt <taylkj04@students.ipfw.edu>
2018-01-23 10:53:09 -0500

    fixed app and map keys. meridian maps now configured correctly for our app.

--- app/src/main/java/edu/ipfw/parkview/indoornavigation/Application.java
+++ app/src/main/java/edu/ipfw/parkview/indoornavigation/Application.java
@@ -6,8 +6,8 @@ import com.arubanetworks.meridian.editor.EditorKey;
 public class Application extends android.app.Application {
 
     //TODO: Put these strings in values/strings.xml
-    public static final EditorKey APP_KEY = EditorKey.forApp("f477d4c144c1541e6452b812bc3b194d4770b6c4");
-    public static final EditorKey MAP_KEY = EditorKey.forMap("6555052652625920", APP_KEY);
+    public static final EditorKey APP_KEY = EditorKey.forApp("6555052652625920");
+    public static final EditorKey MAP_KEY = EditorKey.forMap("5649391675244544", APP_KEY);
 
     @Override
     public void onCreate() {
