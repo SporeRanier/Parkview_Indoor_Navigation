ad6c2428fde5e581b6430f5f21a53b68187dd256
dmcshane01 <ddely@live.com>
2018-01-28 18:14:47 -0500

    Added navigation drawer, refactored to fragment centric design

--- app/src/main/java/edu/ipfw/parkview/indoornavigation/MostVisitedLocations.java
+++ app/src/main/java/edu/ipfw/parkview/indoornavigation/MostVisitedLocations.java
@@ -1,13 +1,20 @@
 package edu.ipfw.parkview.indoornavigation;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MostVisitedLocations extends AppCompatActivity {
+public class MostVisitedLocations extends Fragment {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_most_visited_locations);
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_most_visited_locations, container, false);
+        return view;
     }
 }
--- app/src/main/java/edu/ipfw/parkview/indoornavigation/UpcomingEvents.java
+++ app/src/main/java/edu/ipfw/parkview/indoornavigation/UpcomingEvents.java
@@ -1,13 +1,22 @@
 package edu.ipfw.parkview.indoornavigation;
 
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class UpcomingEvents extends AppCompatActivity {
+/*  must extend android.support.v4.app.Fragment;  */
+public class UpcomingEvents extends Fragment {
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_upcoming_events, container, false);
+        return view;
     }
 }
