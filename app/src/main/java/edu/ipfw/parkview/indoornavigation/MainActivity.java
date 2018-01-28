b68d96297698b23313f1d1599dece3c29c552e38
Palibanwait24 <palibanwait24@gmail.com>
2018-01-27 21:16:29 -0500

    new Main Menu Concept

--- app/src/main/java/edu/ipfw/parkview/indoornavigation/MainActivity.java
+++ app/src/main/java/edu/ipfw/parkview/indoornavigation/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_main_menu);
     }
 
     public void onButtonClick(View v) {
