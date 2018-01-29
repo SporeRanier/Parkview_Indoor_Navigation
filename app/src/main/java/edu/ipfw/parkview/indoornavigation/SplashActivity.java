cb11273ccb77f5b04d8c0a127d9d092f3844f908
dmcshane01 <ddely@live.com>
2018-01-28 21:24:30 -0500

    Fixed ugly merge

--- app/src/main/java/edu/ipfw/parkview/indoornavigation/SplashActivity.java
+++ app/src/main/java/edu/ipfw/parkview/indoornavigation/SplashActivity.java
@@ -1,15 +1,16 @@
 package edu.ipfw.parkview.indoornavigation;
+ 
+    import android.support.v7.app.AppCompatActivity;
+        import android.content.Intent;
+        import android.os.Bundle;
 
-<<<<<<< HEAD:app/src/main/java/edu/ipfw/parkview/indoornavigation/SplashActivity.java
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+        public class SplashActivity extends AppCompatActivity {
 
-public class SplashActivity extends AppCompatActivity {
-
-
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_most_visited_locations, container, false);
-        return view;
-    }
-}
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
