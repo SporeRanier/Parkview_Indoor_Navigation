e6a924227c646699e3aa926a726089af323c3fd9
Palibanwait24 <palibanwait24@gmail.com>
2018-01-30 23:20:53 -0500

    made contact US Page

---
+++ app/src/main/java/edu/ipfw/parkview/indoornavigation/ContactUsActivity.java
@@ -0,0 +1,22 @@
+package edu.ipfw.parkview.indoornavigation;
+
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/*  must extend android.support.v4.app.Fragment;  */
+public class ContactUsActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_contact_us, container, false);
+        return view;
+    }
+}
--- app/src/main/java/edu/ipfw/parkview/indoornavigation/MainActivity.java
+++ app/src/main/java/edu/ipfw/parkview/indoornavigation/MainActivity.java
@@ -103,6 +103,9 @@ public class MainActivity extends AppCompatActivity  {
             case R.id.drawer_upcoming_events:
                 fragmentClass = UpcomingEvents.class;
                 break;
+            case R.id.drawer_contact_us:
+                fragmentClass = ContactUsActivity.class;
+                break;
             case R.id.drawer_wait_time:
                 fragmentClass = WaitTimeFragment.class;
                 break;
@@ -146,5 +149,17 @@ public class MainActivity extends AppCompatActivity  {
             e.printStackTrace();
         }
     }
+    public void onContactUsClick(View v) {
+        try{
 
+            fragmentClass = ContactUsActivity.class;
+            fragment = (Fragment) fragmentClass.newInstance();
+            fragmentManager.beginTransaction().replace(R.id.clMainMenu, fragment).commit();
+            menuItem = (MenuItem)findViewById(R.id.drawer_contact_us);
+            menuItem.setChecked(true);
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
