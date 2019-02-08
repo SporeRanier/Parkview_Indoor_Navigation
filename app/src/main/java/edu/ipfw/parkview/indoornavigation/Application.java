46a38c0b4d84b2fb7124f41ca2820a63263d9815
sporeranier <mjwolgast@gmail.com>
2019-02-08 15:33:44 -0500

    Revert "Adding Picture and description dialog"
    This reverts commit c321a7e

--- app/src/main/java/edu/ipfw/parkview/indoornavigation/Application.java
+++ app/src/main/java/edu/ipfw/parkview/indoornavigation/Application.java
@@ -1,16 +1,7 @@
 package edu.ipfw.parkview.indoornavigation;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.os.Build;
-
 import com.arubanetworks.meridian.Meridian;
 import com.arubanetworks.meridian.editor.EditorKey;
-import com.arubanetworks.meridian.campaigns.CampaignBroadcastReceiver;
-import com.arubanetworks.meridian.campaigns.CampaignsService;
-import com.arubanetworks.meridian.campaigns.Campaign;
-
 
 /* Initialize static and constant fields of our application*/
 public class Application extends android.app.Application {
@@ -20,7 +11,6 @@ public class Application extends android.app.Application {
     public static final String PLACEMARK_UID = "5649391675244544_5738600293466112";
     public static final String CAMPAIGN_ID = "5664248772427776"; // unique id for one of your campaigns here.
     public static final String EDITOR_TOKEN = "f477d4c144c1541e6452b812bc3b194d4770b6c4"; // your editor token here
-    public static final String TEST_CHANNEL_ID = "testchannel";
     public UserData userData;
     static boolean firstStart;
     @Override
@@ -29,22 +19,9 @@ public class Application extends android.app.Application {
         Meridian.configure(this);
         Meridian.getShared().initGoalsForLocation(APP_KEY.toString());
         Meridian.getShared().setEditorToken(EDITOR_TOKEN);
+        CampaignReceiver.CreateNotificationChannel(this);
         super.onCreate();
     }
-
-    /*private void createNotificationChannels() {
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL, "Campaign Notifications", NotificationManager.IMPORTANCE_DEFAULT);
-
-            // Configure the notification channel.
-            notificationChannel.setDescription("Campaign Channel");
-            notificationChannel.enableLights(true);
-            notificationManager.createNotificationChannel(notificationChannel);
-        }
-    }
-    */
 
     public static void setFirstStart() {
         firstStart = false;
